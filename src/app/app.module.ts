90a6f1b85341ac34ab6914477819ce56c53320ac
Ali <ali@cuoma.com>
2022-07-10 18:59:12 -0300

    created layout folder created constants folder moved database to constants change bookdetail name to bookDetail moved bookDetail from app root to shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimelineComponent } from './timeline/timeline.component';
-import { BookdetailComponent } from './bookdetail/bookdetail.component';
+import { BookdetailComponent } from './shared/components/bookdetail/book-detail.component';
 
 @NgModule({
   declarations: [
